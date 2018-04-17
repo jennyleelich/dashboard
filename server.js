e7719b09238eb0b04ab89d0c7bd6c354540dc199
jenny <jenny.lee4567@gmail.com>
2018-04-17 10:18:09 -0400

    adding server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 //Install express server
 const express = require('express');
 const app = express();
+const path = require('path');
+app.get('/*all', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
