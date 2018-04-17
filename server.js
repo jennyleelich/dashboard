969055a5242edaf79a6c0facc619fa9a1658968a
jenny <jenny.lee4567@gmail.com>
2018-04-17 10:47:37 -0400

    change package

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+ });
+
+ console.log('console listening');
